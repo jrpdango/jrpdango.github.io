6a4cbb8600e66aa05906f65b962f715f225c90de
jrpdango <72563699+jrpdango@users.noreply.github.com>
2024-05-29 18:12:55 +0800

    Initial commit

---
+++ src/components/UnderConstruction.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="main-text">
+        <p>This page is currently under construction.</p>
+    </div>
+</template>
+
+<style scoped>
+    .main-text {
+        display: flex;
+        width: 100%;
+        align-items: center;
+    }
+
+    .main-text p {
+        color: whitesmoke;
+    }
+</style>
