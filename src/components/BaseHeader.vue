8333288e892d98e7c89b3382692aaea375a8fc53
jrpdango <72563699+jrpdango@users.noreply.github.com>
2024-06-23 15:42:17 +0800

    Create BaseHeader

---
+++ src/components/BaseHeader.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div>
+        <h3>Jasper Robert Pigason</h3>
+        <div class="social-links">
+            hello
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .social-links {
+
+    }
+</style>
