a0e248cfa712c65b9cdd4e497ebbc9a80a5e20c2
jrpdango <72563699+jrpdango@users.noreply.github.com>
2024-06-23 17:44:02 +0800

    Add styling to header

--- src/components/BaseHeader.vue
+++ src/components/BaseHeader.vue
@@ -1,19 +1,40 @@
 <script setup lang="ts">
+import githubIcon from '/github-color.svg';
+import linkedinIcon from '/linkedin-color.svg';
 defineEmits(['themeChanged']);
 </script>
 
 <template>
     <div>
-        <h3>Jasper Robert Pigason</h3>
-        <button @click="$emit('themeChanged')">Change theme</button>
+        <div class="top-row">
+            <h2>Jasper Robert Pigason</h2>
+            <button class="theme-switcher" @click="$emit('themeChanged')">Change theme</button>
+        </div>
+        
         <div class="social-links">
-            hello
+            <img :src="linkedinIcon" />
+            <img :src="githubIcon" />
         </div>
     </div>
 </template>
 
 <style scoped>
-    .social-links {
+    .top-row {
         display: flex;
+        justify-content: space-between;
+    }
+
+    .theme-switcher {
+        height: fit-content;
+    }
+
+    .social-links {
+        height: 1.75rem;
+        display: flex;
+        margin: 0;
+    }
+
+    .social-links img {
+        margin-right: 8px;
     }
 </style>
