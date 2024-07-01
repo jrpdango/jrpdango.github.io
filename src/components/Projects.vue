16675952814ebac9f10a4b02e415f0d4529f226f
jrpdango <72563699+jrpdango@users.noreply.github.com>
2024-07-01 16:16:26 +0800

    Update basic UI for projects

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -4,14 +4,31 @@ import projects from '../data/projects.ts';
 </script>
 
 <template>
-    <div class="personal-projects">
-        <h3>Personal Projects</h3>
-        <div v-for="project in projects">
+    <h3>Personal Projects</h3>
+    <div class="projects">
+        <div v-for="project in projects" class="project">
             <ProjectCard :name="project.name" :description="project.description" />
         </div>
     </div>
 
-    <div class="personal-projects">
-        <h3>Work Projects</h3>
+    <h3>Work Projects</h3>
+    <div class="projects">
+        
     </div>
 </template>
+
+<style>
+    .projects {
+        margin: 32px 16px 32px 16px;
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        column-gap: 32px;
+        justify-items: center;
+    }
+
+    @media (max-width: 810px) {
+        .personal-projects {
+            grid-template-columns: repeat(2, 1fr);
+        }
+    }
+</style>
