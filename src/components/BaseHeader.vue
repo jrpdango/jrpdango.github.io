a2bfa06e2286ef1795997e895b846e23003378b2
jrpdango <72563699+jrpdango@users.noreply.github.com>
2026-09-13 18:17:56 +0800

    feat: add light mode electric fan sprites

--- src/components/BaseHeader.vue
+++ src/components/BaseHeader.vue
@@ -1,24 +1,36 @@
 <script setup lang="ts">
 import { onMounted, onUnmounted, ref } from "vue";
 
-import south from "@/assets/electric-fan/south.webp";
-import southWest from "@/assets/electric-fan/south-west.webp";
-import west from "@/assets/electric-fan/west.webp";
-import northWest from "@/assets/electric-fan/north-west.webp";
-import north from "@/assets/electric-fan/north.webp";
-import northEast from "@/assets/electric-fan/north-east.webp";
-import east from "@/assets/electric-fan/east.webp";
-import southEast from "@/assets/electric-fan/south-east.webp";
+import ThemedImage from "./ThemedImage.vue";
+import { ThemedImageSource } from "../types";
 
-const frames = [
-    south,
-    southWest,
-    west,
-    northWest,
-    north,
-    northEast,
-    east,
-    southEast,
+import darkSouth from "@/assets/electric-fan/south.webp";
+import darkSouthWest from "@/assets/electric-fan/south-west.webp";
+import darkWest from "@/assets/electric-fan/west.webp";
+import darkNorthWest from "@/assets/electric-fan/north-west.webp";
+import darkNorth from "@/assets/electric-fan/north.webp";
+import darkNorthEast from "@/assets/electric-fan/north-east.webp";
+import darkEast from "@/assets/electric-fan/east.webp";
+import darkSouthEast from "@/assets/electric-fan/south-east.webp";
+
+import lightSouth from "@/assets/electric-fan-light/south.webp";
+import lightSouthWest from "@/assets/electric-fan-light/south-west.webp";
+import lightWest from "@/assets/electric-fan-light/west.webp";
+import lightNorthWest from "@/assets/electric-fan-light/north-west.webp";
+import lightNorth from "@/assets/electric-fan-light/north.webp";
+import lightNorthEast from "@/assets/electric-fan-light/north-east.webp";
+import lightEast from "@/assets/electric-fan-light/east.webp";
+import lightSouthEast from "@/assets/electric-fan-light/south-east.webp";
+
+const frames: ThemedImageSource[] = [
+    { forDark: darkSouth, forLight: lightSouth },
+    { forDark: darkSouthWest, forLight: lightSouthWest },
+    { forDark: darkWest, forLight: lightWest },
+    { forDark: darkNorthWest, forLight: lightNorthWest },
+    { forDark: darkNorth, forLight: lightNorth },
+    { forDark: darkNorthEast, forLight: lightNorthEast },
+    { forDark: darkEast, forLight: lightEast },
+    { forDark: darkSouthEast, forLight: lightSouthEast },
 ];
 
 const frame = ref(0);
@@ -43,12 +55,12 @@ onUnmounted(() => {
     <div class="header">
         <div class="name-row">
             <h1 class="my-name">Jasper Robert Pigason</h1>
-            <img
+            <ThemedImage
                 class="fan"
                 :src="frames[frame]"
                 alt=""
-                width="40"
-                height="40"
+                :width="40"
+                :height="40"
             />
         </div>
         <p class="my-description">
